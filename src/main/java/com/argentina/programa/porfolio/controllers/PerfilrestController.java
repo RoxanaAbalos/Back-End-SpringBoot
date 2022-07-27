7940fbe53557ec420145580332cfce03451ceb5a
RoxanaAbalos <anoeli999@gmail.com>
2022-07-27 13:49:51 -0300

    metodos subir fotos

--- src/main/java/com/argentina/programa/porfolio/controllers/PerfilrestController.java
+++ src/main/java/com/argentina/programa/porfolio/controllers/PerfilrestController.java
@@ -3,14 +3,25 @@ package com.argentina.programa.porfolio.controllers;
 import com.argentina.programa.porfolio.entity.Perfil;
 import com.argentina.programa.porfolio.services.IPerfilService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @CrossOrigin(origins= {"http://localhost:4200","*"})
 @RestController
@@ -56,7 +67,7 @@ public class PerfilrestController {
         }
 
         response.put("mensaje", "Perfil creado con éxito!");
-        response.put("competencia", perfilNew);
+        response.put("perfil", perfilNew);
         return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
@@ -83,7 +94,7 @@ public class PerfilrestController {
         }
 
         response.put("mensaje", "Perfil ha sido actualizado con exito!");
-        response.put("competencia", perfilUpdate);
+        response.put("perfil", perfilUpdate);
         return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
@@ -99,5 +110,51 @@ public class PerfilrestController {
         }
         response.put("mensaje", "Perfil ha sido eliminado con exito!");
         return new ResponseEntity<Map<String,Object>>(response, HttpStatus.OK);
+    }
+    @PostMapping("/perfil/upload")
+    public ResponseEntity<?> upload(@RequestParam("archivo") MultipartFile archivo, @RequestParam("id") Long id) {
+        Map<String, Object> response = new HashMap<>();
+        Perfil perfil = perfilService.findById(id);
+        if (!archivo.isEmpty()) {
+            String nombreArchivo = UUID.randomUUID().toString() + "-" + archivo.getOriginalFilename();
+            Path rutaArchivo = Paths.get("uploads").resolve(nombreArchivo).toAbsolutePath();
+            try {
+                Files.copy(archivo.getInputStream(), rutaArchivo);
+            } catch (IOException e) {
+                response.put("mensaje", "Error al subir la imagen " + nombreArchivo);
+                response.put("error", e.getMessage().concat(": ").concat(e.getCause().getMessage()));
+                return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            String nombreFotoAnterior = perfil.getFoto();
+            if (nombreFotoAnterior != null && nombreFotoAnterior.length() > 0) {
+                Path rutaFotoAnterior = Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();
+                File archivoFotoAnterior = rutaFotoAnterior.toFile();
+                if (archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+                    archivoFotoAnterior.delete();
+                }
+            }
+            perfil.setFoto(nombreArchivo);
+            perfilService.save(perfil);
+            response.put("perfil", perfil);
+            response.put("mensaje", "El Perfil ha sido actualizado con éxito! " + nombreArchivo);
+        }
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/uploads/img/{nombreFoto:.+}")
+    public ResponseEntity<Resource> verFoto(@PathVariable String nombreFoto) {
+        Path rutaArchivo = Paths.get("uploads").resolve(nombreFoto).toAbsolutePath();
+        Resource recurso = null;
+        try {
+            recurso = new UrlResource(rutaArchivo.toUri());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        if (!recurso.exists() && !recurso.isReadable()) {
+            throw new RuntimeException("Error no se pudo cargar la imagen " + nombreFoto);
+        }
+        HttpHeaders cabecera = new HttpHeaders();
+        cabecera.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"");
+        return new ResponseEntity<Resource>(recurso, cabecera, HttpStatus.OK);
     }
 }
